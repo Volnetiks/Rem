67148edc478829324beb317ca5b70e783c8ae2a8
Thomas Bechu <volnetiks@gmail.com>
2019-02-10 10:30:52 +0100

    Command

--- commands/patate.js
+++ commands/patate.js
@@ -2,7 +2,6 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args, sql) => {
     message.channel.send("Patate");
-    message.channel.send("Banane");
     return;
 }
 
