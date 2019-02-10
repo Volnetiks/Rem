216118e16610663d0884d98d168c719b9cc40557
Thomas Bechu <volnetiks@gmail.com>
2019-02-10 10:10:05 +0100

    Updated commands

--- commands/test.js
+++ commands/test.js
@@ -2,6 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args, sql) => {
     message.channel.send("Patate");
+    message.channel.send("Banane")
     return;
 }
 
--- commands/update.js
+++ commands/update.js
@@ -4,18 +4,15 @@ const { exec } = require("child_process")
 module.exports.run = async (bot, message, args, sql) => {
     if(message.author.id === "283164429619691520") {
         const embed = new Discord.RichEmbed()
-        .setTitle("Update")
-        .setColor("#00c7ff")
-        exec('git pull origin master', (error, stdout, stderr) => {
-            if(error) {
-                embed.setDescription('```' + `An error has occured: ${error}` + '```')
-            } else if (stdout.includes("Already up-to-date")) {
-                embed.setDescription("The bot is already up-to-date")
-            } else {
-                embed.setDescription('```Out: ' + stdout + ' Err: ' + stderr + '```')
-            }
-            message.channel.send(embed);
-        })
+            embed.setTitle('Output');
+            embed.setColor('#BA55D3');
+            exec('git pull origin master', (error, stdout, stderr) => {
+                if (error) {embed.setDescription('```' + error + '```'); console.log(error);}
+                else if (stdout.includes('Already up to date')) {embed.setDescription('Bot already up to date!');}
+                else embed.setDescription('```Stdout: ' + stdout + ' stderr: ' + stderr + '```');
+                message.channel.send(embed);
+            });
+        message.channel.send(embed);
     }
 }
 
