3ead447af69eea572d28d8ce854ee55193be1957
Volnetiks - Clémentine <volnetiks@gmail.com>
2020-08-10 16:17:36 +0200

    start bot

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 const { Client } = require("discord.js");
+const { config } = require("dotenv");
 
 const client = new Client({
     disableEveryone: true
+});
+
+config({
+    path: __dirname + "/.env"
+});
+
+client.on("ready", () => {
+    console.log(`${client.user.username} is online`);
+    client.user.setPresence({
+        status: "online",
+        game: {
+            name: "me getting developed",
+            type: "WATCHING"
+        }
+    });
 });
 
 client.login(process.env.token);
