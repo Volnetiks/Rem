7bf0bd46de9970cba10e80dc947080ede4da2caa
Volnetiks - Clémentine <volnetiks@gmail.com>
2020-08-11 11:59:15 +0200

    added functions.js and added profile.js

---
+++ commands/info/profile.js
@@ -0,0 +1,35 @@
+const { RichEmbed } = require("discord.js/src");
+const { stripIndents } = require("common-tags");
+const { getMember, formatDate } = require("../../utils/functions.js");
+
+module.exports = {
+    name: "profile",
+    category: "info",
+    description: "Give information about you or a specific member.",
+    usage: "[username | id | mention]",
+    run: async (client, message, args) => {
+        const member = getMember(message, args.join(" "));
+
+        const joined = formatDate(member.joinedAt);
+        const roles = member.roles.filter(r => r.id !== message.guild.id).map(r => r).join(", ") || "This user have no roles.";
+        const created = formatDate(member.user.createdAt);
+
+        const embed = new RichEmbed()
+            .setFooter(member.displayName, member.user.displayAvatarURL)
+            .setThumbnail(member.user.displayAvatarURL)
+            .setColor(member.displayHexColor === '#000000' ? '#ffffff' : member.displayHexColor)
+            .addField('Member information:', stripIndents`**> Display name:** ${member.displayName}
+            **> Joined at:** ${joined}
+            **> Roles:** ${roles}`, true)
+            .addField('User information:', stripIndents`**> ID:** ${member.user.id}
+            **> Username:** ${member.user.username}
+            **> Tag:** ${member.user.tag}
+            **> Created at:** ${created}`, true)
+            .setTimestamp()
+
+        if (member.user.presence.game) 
+            embed.addField('Currently playing', stripIndents`**> Application:** ${member.user.presence.game.name}`);
+
+        message.channel.send(embed);
+    }
+}
